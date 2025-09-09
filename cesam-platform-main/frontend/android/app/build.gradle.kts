2132f634862f4ac439ecc5cd8f34c6a011566b6c
JerryTheSkull <jerryahoton@gmail.com>
2025-09-09 07:46:46 +0100

    Add files via upload

---
+++ cesam-platform-main/frontend/android/app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.cesam.platform"
+    compileSdk = flutter.compileSdkVersion
+
+    // ✅ NDK version fixée
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.cesam.platform"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    implementation("com.google.android.material:material:1.12.0") // ✅ Ajout pour Material Components
+    implementation("com.google.firebase:firebase-messaging:23.1.2")
+}
+
+apply(plugin = "com.google.gms.google-services")
---
+++ cesam-platform-main/frontend/android/app/src/main/kotlin/com/cesam/platform/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.cesam.platform
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
